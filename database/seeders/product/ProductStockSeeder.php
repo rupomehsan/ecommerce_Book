2d98888842d7097aabe1f6438b669257283004a3
Md-shefat-masum <mshefat924@gmail.com>
2023-11-06 18:29:24 +0600

    stock seeder modify

--- database/seeders/product/ProductStockSeeder.php
+++ database/seeders/product/ProductStockSeeder.php
@@ -13,34 +13,6 @@ class ProductStockSeeder extends Seeder
      */
     public function run(): void
     {
-        ProductStock::truncate();
-        ProductStock::create([
-            'product_id'=>'1',
-            'qty'=>'4',
-            'purchase_date'=>'2023-11-10 20:59:00',
-           
-        ]);
-        ProductStock::create([
-            'product_id'=>'2',
-            'qty'=>'5',
-            'purchase_date'=>'2023-11-11 20:59:00',
-           
-        ]); ProductStock::create([
-            'product_id'=>'3',
-            'qty'=>'7',
-            'purchase_date'=>'2023-11-12 20:59:00',
-           
-        ]); ProductStock::create([
-            'product_id'=>'4',
-            'qty'=>'9',
-            'purchase_date'=>'2023-11-13 20:59:00',
-           
-        ]); ProductStock::create([
-            'product_id'=>'5',
-            'qty'=>'10',
-            'purchase_date'=>'2023-11-14 20:59:00',
-           
-        ]);
-     
+
     }
 }
